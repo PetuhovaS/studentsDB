18e07712346632948ec22d3431e7b38578c1c5ec
Svetlana <lipa8881@rambler.ru>
2017-10-23 12:14:56 +0300

    111

--- students/views.py
+++ students/views.py
@@ -1,8 +1,26 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def students_list(request):
-      return render(request, 'students/students_list.html', {})
+      students = (
+            {'id': 1,
+      'first_name': u'Віталій',
+      'last_name': u'Подоба',
+      'ticket': 235,
+      'image': 'img/1.jpeg'},
+      {'id': 2,
+      'first_name': u'Корост',
+      'last_name': u'Андрій',
+      'ticket': 2123,
+      'image': 'img/2.jpeg'},
+      {'id': 3,
+       'first_name': u'Светлана',
+       'last_name': u'Петухова',
+       'ticket': 235,
+       'image': 'img/3.jpeg'},
+      )
+      return render(request, 'students/students_list.html', {'students': students})
 
 def students_add(request):
       return HttpResponse('<h1>Student Add Form</h1>')
